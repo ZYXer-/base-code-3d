a481431a08cfa4d3e1ed1d0a7850922a2dc89b96
Jan Meier <janmeier0.o@gmail.com>
2015-01-21 19:35:20 +0100

    improved hexagon tile map

--- js/hexagon_map.js
+++ js/hexagon_map.js
@@ -1,14 +1,15 @@
 function Map() {
+    // http://keekerdc.com/2011/03/hexagon-grids-coordinate-systems-and-distance-calculations/
 
     this.radius = 0;
     this.size = 0;
-    this.centerPosition;
+    this.centerPosition = null;
     this.tiles = [];
 
     this.init = function (radius) {
         this.size = radius * 2 + 1;
         this.radius = radius;
-        this.centerPosition = new Position2D.makePosition(radius-1, radius-1);
+        this.centerPosition = new Position2D.makePosition(radius, radius);
 
         for (var x = 0; x < this.size; x++) {
             this.tiles[x] = [];
@@ -40,13 +41,32 @@ function Map() {
     };
 
 
+    this.getAllValidTiles = function () {
+        var result = [];
+        for (var x = 0; x < this.size; x++) {
+            for (var y = 0; y < this.size; y++) {
+                var tile = this.getTile(x, y);
+                if (tile) {
+                    result.push(tile);
+                }
+            }
+        }
+        return result;
+    };
+
+
     this.calculateDistanceVector = function (startPoint, endPoint) {
         var distanceX = endPoint.x - startPoint.x;
         var distanceY = endPoint.y - startPoint.y;
-        var distanceZ = -(distanceX + distanceY);
+        var distanceZ = endPoint.getZ() - startPoint.getZ();
         return Vector3D.makeVector(distanceX, distanceY, distanceZ);
 
-    }
+    };
+
+
+    this.getDistanceToCenter = function (point) {
+        return this.getDistance(point, this.centerPosition);
+    };
 
 
     this.getDistance = function (startPoint, endPoint) {
@@ -64,8 +84,7 @@ function Map() {
             return false;
         }
 
-        var tilePosition = new Position2D();
-        tilePosition.init(x, y);
+        var tilePosition = Position2D.makePosition(x, y);
 
         var distance = this.getDistance(tilePosition, this.centerPosition);
 
@@ -77,9 +96,9 @@ function Map() {
         if (!this.doesTileExist(x, y)) {
             return [];
         }
-        var targetTilePosition = Position2D.makePosition(x,y);
+        var targetTilePosition = Position2D.makePosition(x, y);
 
-        var halfStep = radius - Math.floor(radius) != 0;
+        var halfStep = (radius - Math.floor(radius)) != 0;
         radius = Math.ceil(radius);
         var result = [];
 
@@ -92,14 +111,14 @@ function Map() {
             for (var indexY = startY; indexY <= endY; indexY++) {
                 var position = Position2D.makePosition(indexX, indexY);
                 var distance = this.getDistance(position, targetTilePosition);
-                if ( distance <= radius) {
-                    if (distance == radius && halfStep){
+                if (distance <= radius) {
+                    if (distance == radius && halfStep) {
                         var distanceVector = this.calculateDistanceVector(position, targetTilePosition);
-                        if (!distanceVector.isOrthogonalToBaseVector()){
+                        if (!distanceVector.isOrthogonalToBaseVector()) {
                             result.push(this.getTile(indexX, indexY));
                         }
                     }
-                    else{
+                    else {
                         result.push(this.getTile(indexX, indexY));
                     }
                 }
@@ -108,5 +127,54 @@ function Map() {
 
         return result;
     };
+
+    this.pointInOrthogonalCoordinates = function (position) {
+        var xCoordinates = position.x - ((position.y) * 0.5);
+        var yCoordinates = position.y * 0.75;
+        return Position2D.makePosition(xCoordinates, yCoordinates)
+    }
 }
+
+
+DrawableMap = function () {
+    this.map = null;
+    this.scene = null;
+    this.tiles = [];
+
+
+    this.init = function (map, scene) {
+        this.map = map;
+        this.scene = scene;
+
+    };
+
+
+    this.update = function (timeDelta) {
+
+        var timeDelta =  0.01;
+
+        for (var index = 0; index < this.tiles.length; index++){
+           this.tiles[index].update(timeDelta);
+        }
+    };
+
+
+    this.createTiles = function () {
+        var tiles = this.map.getAllValidTiles();
+        for (var index = 0; index < tiles.length; index++) {
+            var tile = tiles[index];
+            if (tile) {
+                this.addTile(tile);
+            }
+        }
+
+    };
+
+
+    this.addTile = function (tile) {
+        var hexagonTile = new DrawableHexagonTile(tile);
+        this.scene.add(hexagonTile.getShape());
+        this.tiles.push(hexagonTile);
+    };
+};
 
--- js/tile.js
+++ js/tile.js
@@ -11,6 +11,11 @@ function Position2D() {
       this.x = x;
       this.y = y;
    };
+
+   this.getZ = function(){
+      return -(this.x - this.y);
+
+   }
 }
 
 Position2D.makePosition = function(x, y) {
@@ -50,11 +55,121 @@ function Tile() {
    this.position = new Position2D();
 
    this.map;
+   this.uniforms;
 
 
    this.init = function(x, y, map) {
       this.map = map;
       this.position.init(x,y)
    };
+}
+
+function DrawableHexagonTile(tile) {
+   this.tile = tile;
+   this.shape = null;
+
+
+   this.update = function(delta){
+      var uniforms = this.getUniforms();
+      uniforms.time.value += delta;
+      uniforms.uCol.value = new THREE.Color( 0xffaa00 );
+      uniforms.amplitude.value = Math.sin(timer.lastTime * 0.0005)*0.2;
+   };
+
+
+   this.getShape = function(){
+      if (! this.shape){
+         this.shape = this.createShape();
+      }
+      return this.shape;
+   };
+
+
+   this.createShape = function(){
+      var material = this.getMaterial();
+      var hexagon = Shapes3D.makeHexagon(material);
+
+      var tPosition = this.getTranslatedPosition();
+      hexagon.position.set(tPosition.x - this.tile.map.radius, 1, tPosition.y - this.tile.map.radius);
+      return hexagon;
+
+   };
+
+
+   this.getTranslatedPosition = function(){
+      if (!this.translatedPosition) {
+         this.translatedPosition = this.tile.map.pointInOrthogonalCoordinates(this.tile.position);
+      }
+      return this.translatedPosition;
+   };
+
+
+   this.getColorCode = function() {
+      var distance = this.tile.map.getDistanceToCenter(this.tile.position);
+      var color;
+      if (distance == 0) {
+         color = 0x000000;
+      }
+      else if (distance == 1) {
+         color = 0xFF0000;
+      }
+      else if (distance == 2) {
+         color = 0x00FF00;
+      }
+      else if (distance == 3) {
+         color = 0x0000FF;
+      }
+      else if (distance == 4) {
+         color = 0xFF00FF;
+      }
+      else {
+         color = Math.random() * 0xffffff;
+      }
+      return color;
+
+   };
+
+
+   this.getMaterial = function(){
+      var material = new THREE.ShaderMaterial({
+         uniforms: this.getUniforms(),
+         attributes: this.getAttributes(),
+         vertexShader: shaders.floatingHexagon.vertex,
+         fragmentShader: shaders.floatingHexagon.fragment
+      });
+      return material;
+   };
+
+
+   this.getUniforms = function(){
+      if (!this.uniforms) {
+         this.uniforms = {
+            time: {type: "f", value: 0},
+            "uCol" : { type: "c", value: new THREE.Color( 0xffaa00 ) },
+            amplitude: {type: "f", value: 0}
+         };
+      }
+      return this.uniforms;
+   };
+
+
+   this.getAttributes = function(){
+      if (!this.attributes) {
+         this.attributes = {
+            "aX": {type: 'f', value: []},
+            "aY": {type: 'f', value: []},
+            "aCol" : { type: "c", value:[]}
+         };
+
+         var tPosition = this.getTranslatedPosition();
+         var color = this.getColorCode();
+         for (var v = 0; v < 10; v++) {
+            this.attributes.aCol.value.push(new THREE.Color(color));
+            this.attributes.aX.value.push(tPosition.x);
+            this.attributes.aY.value.push(tPosition.y);
+         }
+      }
+      return this.attributes;
+   }
 }
 
